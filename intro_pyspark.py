f6378a3af27ab39d3a92f2b8947b498e2c9e9703
Roman Rieunier <rieunierroman@hotmail.fr>
2019-12-02 02:28:50 +0100

    Updated .gitignore and parsed types in intro pyspark

--- intro_pyspark.py
+++ intro_pyspark.py
@@ -6,45 +6,27 @@ sc = SparkContext()
 
 rddStr = sc.textFile('data/worldcitiespop.txt').cache()
 print(rddStr.count())
-# Cleaning populations
-rddStr = rddStr.map(lambda x: x.split(','))
-# print(rddStr.count())
 
+# Cleaning populations and applying proper types
+rddStr = rddStr.map(lambda x: x.split(','))
 header = rddStr.first()
 rddStr = rddStr.filter(lambda x: x[4] != '' and x != header)
-# print(rddStr.count())
+rddStr = rddStr.map(lambda x: [x[0], x[1], x[2], x[3], int(x[4]), float(x[5]), float(x[6])])
+
+
 # print(*rddStr.take(30), sep='\n')
 
-test = rddStr.take(10)
-print(*test[0], sep="\t")
-print(*test[1], sep="\t")
-
-
-def mkdict(t):
+def mk_pop_dict(t):
     return {"_doc": dict((zip(["city", "country", "localisation", "dept", "pop"],
-                              [t[1], t[0], {"lat": float(t[5]), "lon": float(t[6])}, t[3], float(t[4])])))}
+                              [t[1], t[0], {"lat": t[5], "lon": t[6]}, t[3], t[4]])))}
 
 
-clean_cities = rddStr.map(mkdict).collect()
+clean_cities = rddStr.map(mk_pop_dict).collect()
 
 with open("data/clean_cities.json", "w") as fw:
     for city in clean_cities:
         fw.write('{"index": {"_index": "anavis"}}\n')
         fw.write(json.dumps(city) + "\n")
 
-# for t in test[1:]:
-#     print(dict((zip(["city", "country", "localisation", "dept", "pop"],
-#                     [t[1], t[0], {"lat": t[5], "lon": t[6]}, t[3], t[4]]))))
-
-# {
-#     "city": "Paris",
-#     "country": "fr",
-#     "localisation: {"lat": 45, "lon": 0 },
-#     "dept" : 75,
-#     "pop" : 100000
-# }
-
-# test.foreach(lambda x: print(x))
-
 # Display stats
-# rddStr.stdev()
+# rddStr.stats()
